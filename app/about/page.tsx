b4acb8e0aea52b4bac21b898351b47ba3d3c947d
Matthew Robinson <robinson.matthew@gmail.com>
2025-10-14 13:28:40 +0200

    Switch all images to external CDN URLs for faster loading and to avoid GitHub file size limits

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -124,7 +124,7 @@ export default function AboutPage() {
             <div className="bg-warm-gray/10 p-8 text-center">
               <div className="relative w-48 h-48 mx-auto mb-6">
                 <Image
-                  src="/images/profile.png"
+                  src="https://i.ibb.co/2Yd8zqy1/profile.png"
                   alt="Matthew James - Artist Portrait"
                   fill
                   className="object-cover rounded-full"
